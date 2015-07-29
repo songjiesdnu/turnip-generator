034e0b994d1f27042dc849df02ea878fdda5d989
songjie <songjiesdnu@163.com>
2015-07-29 18:30:12 +0800

    初始化

---
+++ src/main/java/com/robot/turnip/dao/UserMapper.java
@@ -0,0 +1,16 @@
+package com.robot.turnip.dao;
+
+import com.robot.turnip.domain.User;
+import java.util.List;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    List<User> selectAll();
+
+    int updateByPrimaryKey(User record);
+}
